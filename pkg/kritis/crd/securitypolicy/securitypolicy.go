33f471731e75c928be7d7b9439036caf1601e067
Ryuzo Yamamoto <ryuzo.yamamoto@gmail.com>
2019-02-18 14:16:58 +0900

    [WIP] Add "buildProjectIDs" to ImageSecurityPolicy and check ProjectId of BuildProvenance in Build occurrence

--- pkg/kritis/crd/securitypolicy/securitypolicy.go
+++ pkg/kritis/crd/securitypolicy/securitypolicy.go
@@ -19,6 +19,7 @@ package securitypolicy
 import (
 	"fmt"
 
+	"github.com/golang/glog"
 	"github.com/grafeas/kritis/pkg/kritis/apis/kritis/v1beta1"
 	clientset "github.com/grafeas/kritis/pkg/kritis/client/clientset/versioned"
 	"github.com/grafeas/kritis/pkg/kritis/constants"
@@ -118,6 +119,38 @@ func ValidateImageSecurityPolicy(isp v1beta1.ImageSecurityPolicy, image string, 
 			reason:        SeverityReason(image, v, isp),
 		})
 	}
+
+	// TODO(dragon3): check build occurences
+	glog.Infof("isp.Spec.BuildProjectIDs = %v", isp.Spec.BuildProjectIDs)
+	if len(isp.Spec.BuildProjectIDs) > 0 {
+		builds, err := client.Builds(image)
+		if err != nil {
+			return nil, err
+		}
+		hasBuildProjectID := false
+		for _, projectID := range isp.Spec.BuildProjectIDs {
+			for _, build := range builds {
+				if build.Provenance.ProjectID == projectID {
+					hasBuildProjectID = true
+					break
+				}
+			}
+			if hasBuildProjectID {
+				break
+			}
+		}
+		if !hasBuildProjectID {
+			violations = append(
+				violations,
+				NewViolation(
+					nil,
+					policy.BuildProjectIDViolation,
+					policy.Reason(fmt.Sprintf("%s doesn't have build occurence with required projectIDs", image)),
+				),
+			)
+		}
+	}
+
 	return violations, nil
 }
 
